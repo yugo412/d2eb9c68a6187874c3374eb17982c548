db547dbd4849a66e27b51382aeb887c96be6e6f5
Yugo <dedy.yugo.purwanto@gmail.com>
2024-07-15 00:58:27 +0700

    Custom header implementation

--- src/Http/ServerRequest.php
+++ src/Http/ServerRequest.php
@@ -7,5 +7,23 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class ServerRequest extends LaminasServerRequest implements ServerRequestInterface
 {
+    private array $normalizedHeaders = [];
 
+    public function getHeaders(): array
+    {
+        $this->headers = getallheaders();
+
+        foreach ($this->headers as $name => $value) {
+            $this->normalizedHeaders[strtolower($name)] = $value;
+        }
+
+        return $this->headers;
+    }
+
+    public function getHeaderLine(string $name): string
+    {
+        $this->getHeaders();
+
+        return $this->normalizedHeaders[strtolower($name)] ?? '';
+    }
 }
